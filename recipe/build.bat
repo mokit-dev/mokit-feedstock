fb7ceb36391dde4e6393676a22e77be84cddb84d
Shirong_Wang <srwang20@fudan.edu.cn>
2026-02-06 16:24:04 +0800

    debug

--- recipe/build.bat
+++ recipe/build.bat
@@ -2,6 +2,14 @@
 setlocal enabledelayedexpansion
 
 cd src
+echo FC=%FC%
+echo F77=%F77%
+echo F90=%F90%
+echo CC=%CC%
+echo CXX=%CXX%
+echo MAKE=%MAKE%
+echo BUILD_PREFIX=%BUILD_PREFIX%
+echo PREFIX=%PREFIX%
 %PYTHON% -m numpy.f2py -h >NUL 2>&1
 
 make all -f Makefile.gnu_openblas_conda
