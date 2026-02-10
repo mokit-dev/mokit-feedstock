e54f8be61f12096f8daa1854cdca8d23c6e1b653
hebrewsnabla <srwang20@fudan.edu.cn>
2026-02-10 21:21:28 +0800

    start debug

--- recipe/build.bat
+++ recipe/build.bat
@@ -19,17 +19,18 @@ echo CONDA_PREFIX=%CONDA_PREFIX%
 @REM where libgfortran-*.dll
 @REM where libgcc_s_seh-1.dll
 @REM where libwinpthread-1.dll
-@REM where libquadmath-0.dll
-@REM where libgomp-1.dll
-@REM dir /b "%PREFIX%\Library\bin\libgfortran*.dll" 2>NUL
-@REM dir /b "%PREFIX%\Library\bin\libgcc_s_seh-1.dll" 2>NUL
-@REM dir /b "%PREFIX%\Library\bin\libwinpthread-1.dll" 2>NUL
-@REM dir /b "%BUILD_PREFIX%\Library\bin\libgfortran*.dll" 2>NUL
-@REM dir /b "%BUILD_PREFIX%\Library\bin\libgcc_s_seh-1.dll" 2>NUL
-@REM dir /b "%BUILD_PREFIX%\Library\bin\libwinpthread-1.dll" 2>NUL
-@REM dir /b "%BUILD_PREFIX%\Library\bin\libquadmath-0.dll" 2>NUL
-@REM dir /b "%BUILD_PREFIX%\Library\bin\libgomp-1.dll" 2>NUL
-@REM if not "x%OBJDUMP%"=="x" %OBJDUMP% -p "%BUILD_PREFIX%\Library\bin\libgfortran-5.dll" | findstr DLL
+echo PATH=%PATH%
+echo BUILD_PREFIX=%BUILD_PREFIX%
+echo PREFIX=%PREFIX%
+dir /b "%PREFIX%\Library\bin\libgfortran*.dll" 2>NUL
+dir /b "%PREFIX%\Library\bin\libgcc_s_seh-1.dll" 2>NUL
+dir /b "%PREFIX%\Library\bin\libwinpthread-1.dll" 2>NUL
+dir /b "%PREFIX%\Library\bin\libquadmath-0.dll" 2>NUL
+dir /b "%BUILD_PREFIX%\Library\bin\libgfortran*.dll" 2>NUL
+dir /b "%BUILD_PREFIX%\Library\bin\libgcc_s_seh-1.dll" 2>NUL
+dir /b "%BUILD_PREFIX%\Library\bin\libwinpthread-1.dll" 2>NUL
+dir /b "%BUILD_PREFIX%\Library\bin\libquadmath-0.dll" 2>NUL
+if not "x%OBJDUMP%"=="x" %OBJDUMP% -p "%BUILD_PREFIX%\Library\bin\libgfortran-5.dll" | findstr DLL
 %PYTHON% -m numpy.f2py -h >NUL 2>&1
 
 if exist "..\MANIFEST.in" (
@@ -50,6 +51,10 @@ copy /Y "%RECIPE_DIR%\Makefile.gnu_openblas_conda.win" Makefile.gnu_openblas_con
 powershell -Command "$content = Get-Content Makefile.main; $content = $content -replace '\$\(F90\) -shared \$\(FFLAGS\) \$\(MKL_FLAGS\) -o librest2fch\.so \$\(OBJ_py2fch\)', '\$(F90) -shared \$(FFLAGS) -o librest2fch.so \$(OBJ_py2fch) \$(MKL_FLAGS)'; $content = $content -replace 'librest2fch\.so', 'librest2fch.dll'; $content = $content -replace '\.so', '.pyd'; $content = $content -replace '@mv ', '@move '; Set-Content Makefile.main $content"
 
 make exe -f Makefile.gnu_openblas_conda.win
+for /f "delims=" %%F in ('dir /s /b "%SRC_DIR%\src\f2pytmp\bbdir\meson-private\sanitycheckf.exe" 2^>NUL') do (
+  echo --- %%F
+  if not "x%OBJDUMP%"=="x" %OBJDUMP% -p "%%F" | findstr DLL
+)
 set BUILD_ERROR=%ERRORLEVEL%
 if not %BUILD_ERROR%==0 (
   echo Make failed with %BUILD_ERROR%
