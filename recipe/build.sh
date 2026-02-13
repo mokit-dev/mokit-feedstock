43ce4739e27beb38a7fe7273d08a853903d1cb96
Shirong Wang <srwang20@fudan.edu.cn>
2026-02-13 12:11:37 +0800

    revert

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@ set -x -e
 cd src
 echo `which f2py`
 sed -i '' 's/-lgomp//g' Makefile.gnu_openblas_conda
-make all -f Makefile.gnu_openblas_conda || exit 1
+make all -f Makefile.gnu_openblas_conda
 cd ..
 pip install -v --prefix=$PREFIX .
 ls $PREFIX/bin
