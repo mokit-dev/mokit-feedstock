47010f0884af6baf32252f27319aac95926f0803
Shirong Wang <srwang20@fudan.edu.cn>
2026-04-05 16:48:11 +0800

    Modify build.sh to update MANIFEST.in
    Update build script to modify MANIFEST.in for dynamic libraries.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@ cd src
 echo `which f2py`
 sed -i '' 's/-lgomp//g' Makefile.gnu_openblas_conda
 sed -i '' 's/rest2fch.so/rest2fch.dylib/g' Makefile.main
+sed -i '' 's/dll/dll *.dylib/g' ../MANIFEST.in
 make all -f Makefile.gnu_openblas_conda
 cd ..
 pip install -v --prefix=$PREFIX .
