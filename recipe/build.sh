1cf860fce7ccecc3cfa1fd53e81d6f5e725c4fe6
jxzou <njumath@sina.cn>
2026-05-06 17:35:35 +0800

    change pip to python -m pip for MACOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,6 @@ sed -i '' 's/rest2fch.so/rest2fch.dylib/g' Makefile.main
 sed -i '' 's/dll/dll *.dylib/g' ../MANIFEST.in
 make all -f Makefile.gnu_openblas_conda
 cd ..
-pip install -v --prefix=$PREFIX .
+python -m pip install -v --prefix=$PREFIX .
 ls $PREFIX/bin
 mv bin/* $PREFIX/bin
